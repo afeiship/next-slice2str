5ad27965a8aa6f1da0ee26dfd1d166d8b7c20c3d
afeiship <1290657123@qq.com>
2023-07-15 09:10:22 +0800

    feat: add sep company

--- __tests__/index.spec.js
+++ __tests__/index.spec.js
@@ -2,25 +2,37 @@
   require('../src');
 
   describe('api.basic test', () => {
+    test('str no separator use default index', function () {
+      const str = 'abc';
+      const res = nx.slice2str(str, '@');
+      expect(res).toEqual(['', 'abc']);
+    });
+
+    test('str no separator use input index', function () {
+      const str = 'abc';
+      const res = nx.slice2str(str, 2);
+      expect(res).toEqual(['ab', 'c']);
+    })
+
     test('nx.slice2str default step is 1', function () {
-      var str = 'abc@21.cn';
+      const str = 'abc@21.cn';
       const res = nx.slice2str(str, '@');
       expect(res).toEqual(['abc', '21.cn']);
     });
 
     test('nx.slice2str with step1', function () {
-      var str = 'test@123.com';
-      var idx = str.indexOf('@');
-      var arr = nx.slice2str(str, idx, 1);
+      const str = 'test@123.com';
+      const idx = str.indexOf('@');
+      const arr = nx.slice2str(str, idx, 1);
 
       expect(arr[0]).toBe('test');
       expect(arr[1]).toBe('123.com');
     });
 
     test('nx.slice2str with step 0', function () {
-      var str = 'test@123.com';
-      var idx = str.indexOf('@');
-      var arr = nx.slice2str(str, idx, 0);
+      const str = 'test@123.com';
+      const idx = str.indexOf('@');
+      const arr = nx.slice2str(str, idx, 0);
 
       expect(arr[0]).toBe('test');
       expect(arr[1]).toBe('@123.com');
--- src/index.js
+++ src/index.js
@@ -1,10 +1,12 @@
 import nx from '@jswork/next';
 
 nx.slice2str = function (inString, inTarget, inStep) {
-  const idx = typeof inTarget === 'string' ? inString.indexOf(inTarget) : inTarget;
-  const step = typeof inStep === 'undefined' ? 1 : inStep;
+  const isSeparator = typeof inTarget === 'string';
+  const idx = isSeparator ? inString.indexOf(inTarget) : inTarget;
+  const defaultStep = isSeparator ? 1 : 0;
+  const step = typeof inStep === 'undefined' ? defaultStep : inStep;
   if (!inString && inString.length <= idx) return;
-  return [inString.substr(0, idx), inString.substr(idx + step)];
+  return [inString.substr(0, idx), inString.substring(idx + step)];
 };
 
 if (typeof module !== 'undefined' && module.exports && typeof wx === 'undefined') {
